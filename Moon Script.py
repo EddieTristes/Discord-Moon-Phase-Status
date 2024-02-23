c00ae34e00921b90b195427d0e12a579893b1012
Eddie <101036809+EddieTristes@users.noreply.github.com>
2024-02-23 15:41:50 -0600

    Update Moon Script.py
    Changes logic and adds a cleaner output.

--- Moon Script.py
+++ Moon Script.py
@@ -4,7 +4,7 @@ import json
 import time
 from datetime import datetime, timedelta
 
-token = "" #Put your Discord Token here. You can learn how at https://github.com/Tyrrrz/DiscordChatExporter/blob/master/.docs/Token-and-IDs.md
+token = ""  # Put your Discord Token here. You can learn how at https://github.com/Tyrrrz/DiscordChatExporter/blob/master/.docs/Token-and-IDs.md
 
 iteration_count = 1
 
@@ -16,11 +16,8 @@ def get_moon_phase():
     illumination = moon.moon_phase * 100
 
     next_new_moon_date = ephem.next_new_moon(observer.date)
-
     next_first_quarter_date = ephem.next_first_quarter_moon(observer.date)
-
     next_full_moon_date = ephem.next_full_moon(observer.date)
-
     next_last_quarter_date = ephem.next_last_quarter_moon(observer.date)
 
     if observer.date == next_new_moon_date:
@@ -32,15 +29,9 @@ def get_moon_phase():
     elif observer.date == next_full_moon_date:
         return "Full Moon"
     elif observer.date < next_full_moon_date and observer.date < next_new_moon_date:
-        if illumination < 50:
-            return "Waxing Crescent"
-        elif illumination > 50:
-            return "Waxing Gibbous"
+        return "Waxing Crescent" if illumination < 50 else "Waxing Gibbous"
     elif observer.date > next_full_moon_date and observer.date > next_new_moon_date:
-        if illumination < 50:
-            return "Waning Crescent"
-        elif illumination > 50:
-            return "Waning Gibbous"
+        return "Waning Crescent" if illumination < 50 else "Waning Gibbous"
 
 def resolve_icon(phase_name):
     emoji_dict = {
@@ -74,20 +65,26 @@ def update_custom_status(message, emoji):
 
 def perform_initial_update():
     phase_name = get_moon_phase()
-    print("Current moon phase:", phase_name)
-
+    print("===== Initial Update =====")
+    
+    phase_name = get_moon_phase()
     observer = ephem.Observer()
     observer.date = ephem.now()
     moon = ephem.Moon(observer)
     moon.compute()
 
-    illumination_percentage = int(moon.moon_phase * 100)
-    message = f"Tonight's phase is a {phase_name}! The moon's illumination is {illumination_percentage}%"
+    illumination_percentage = moon.moon_phase * 100
+    print("Current moon illumination:", illumination_percentage)
+    print("Current moon phase:", phase_name)
+    illumination_formatted = "{:.1f}".format(illumination_percentage)
+
+    message = f"Tonight's phase is a {phase_name}! The moon's illumination is {illumination_formatted}%"
     emoji = resolve_icon(phase_name)
 
     print("Updating custom status...")
     update_custom_status(message, emoji)
     print("Custom status updated.")
+    print("==========================")
 
 def main():
     global iteration_count
@@ -97,32 +94,34 @@ def main():
         now = datetime.now() - timedelta(hours=6)
 
         if now.minute == 0:
+            print("===== Hourly Update =====")
             print("Checking for moon phase update...")
 
             phase_name = get_moon_phase()
-            print("Current moon phase:", phase_name)
-
             observer = ephem.Observer()
             observer.date = ephem.now()
             moon = ephem.Moon(observer)
             moon.compute()
 
-            illumination_percentage = int(moon.moon_phase * 100)
-            message = f"Tonight's phase is a {phase_name}! The moon's illumination is {illumination_percentage}%"
+            illumination_percentage = moon.moon_phase * 100
+            print("Current moon illumination:", illumination_percentage)
+            print("Current moon phase:", phase_name)
+            illumination_formatted = "{:.1f}".format(illumination_percentage)
+
+            message = f"Tonight's phase is a {phase_name}! The moon's illumination is {illumination_formatted}%"
             emoji = resolve_icon(phase_name)
 
             print("Updating custom status...")
             update_custom_status(message, emoji)
             print("Custom status updated! Waiting an hour to change status...")
+            print("==========================")
 
             time.sleep(3600)
-
             iteration_count = 1
         else:
             print(f"(Iteration {iteration_count}) Not the beginning of the hour. Checking again in a minute...")
             time.sleep(60)
-
-        iteration_count += 1
+            iteration_count += 1
 
 if __name__ == "__main__":
     main()
